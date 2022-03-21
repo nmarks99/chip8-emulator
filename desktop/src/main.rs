ba20bbdb5f6f2931af89c0af54e4f179642681e7
Nick <nmarks99@gmail.com>
2022-03-21 11:01:42 -0500

    add cmd line args to desktop frontend

--- desktop/src/main.rs
+++ desktop/src/main.rs
@@ -1,3 +1,10 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<_> = env::args().collect();
+    if args.len() != 2 {
+        println!("Usage: cargo run path/to/game");
+        return;
+    }
+    
 }
